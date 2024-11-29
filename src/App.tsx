8905e8acb47856a365c87a1db84c578de07bb3de
IsuruUDSilva <isuruudsilva@gmail.com>
2024-11-29 14:49:19 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SignIn from "./pages/auth/signin";
+
+function App() {
+
+  return (
+    <Router>
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<SignIn />} />
+      </Routes>
+    </div>
+  </Router>
+  )
+}
+
+export default App
---
+++ src/pages/auth/signin.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Input } from '../../components/input'
+
+type Props = {}
+
+const SignIn = (props: Props) => {
+  return (
+    <div className='bg-primary-bg h-screen w-screen place-content-center justify-items-center'>
+    <div className='bg-primary h-3/4 w-3/4 rounded-lg shadow-xl'>
+    </div>
+    </div>
+  )
+}
+
+export default SignIn
